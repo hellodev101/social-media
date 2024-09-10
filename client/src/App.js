a511f2ec4f66402db08e1799bd23c0ecff537368
hellodev101 <soraya.preechadech@gmail.com>
2024-09-10 19:46:51 +0200

    Frontend Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid2 } from '@mui/material';
 
-function App() {
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <Container maxWidth="lg">
+      {/* "sx" prop allows developers to define styles using a concise syntax */}
+      <AppBar  position='static' color="inherit" sx={{ display: 'flex', flexDirection: 'row' }} >
+          <Typography variant='h2' align='center'>Instraverse</Typography>
+          <img src={instraverse} alt='Instraverse' height='60'></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+        <Grid2 container justifyContent="space-between" alignItems="stretch" spacing={4}>
+            <Grid2 item xs={12} sm={7}>
+              <Posts/>
+            </Grid2>            
+            <Grid2 item xs={12} sm={4}>
+              <Form/>
+            </Grid2>
+          </Grid2>
+        </Container>
+
+      </Grow>
+     </Container>
+  )
 }
 
 export default App;
